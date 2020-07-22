c8ba981a4b89a433e0b999df9ddc03f5087aa134
keisei77 <keisei.jia@qq.com>
2020-07-22 23:08:36 +0800

    :hammer: add check for argument

--- bin/commit_msg.sh
+++ bin/commit_msg.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+set -e
+
 problem=$@
-git add .
-git commit -m ":white_check_mark: $problem"
-git push
+
+if [[ $@ == "" ]]; then
+    echo "Please enter the problem name."
+else
+    git add .
+    git commit -m ":white_check_mark: $problem"
+    git push
+fi
