cbba5be5d7dc628031b21372b8fb2b3fa3a46278
keisei77 <keisei.jia@qq.com>
2020-07-27 23:02:48 +0800

    :hammer: update commit script

--- bin/commit_msg.sh
+++ bin/commit_msg.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -e
+# https://stackoverflow.com/a/5238537/6796500
+file=$(git status --porcelain | sed s/^...//)
 
-problem=$@
-
-if [[ $@ == "" ]]; then
-    echo "Please enter the problem name."
+if [[ $file == "" ]]; then
+    echo "No files changed."
 else
+
     git add .
-    git commit -m ":white_check_mark: $problem"
-    git push
+    git commit -m ":white_check_mark: $file"
+    # git push
 fi
